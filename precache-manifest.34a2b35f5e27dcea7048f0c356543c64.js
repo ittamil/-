87887c3d5a628cb56f49e53d40082c763210b92a
harih4ran <hariharank.offl@gmail.com>
2022-12-21 22:37:11 +0530

    Updates

---
+++ precache-manifest.34a2b35f5e27dcea7048f0c356543c64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "64cde72354a8fc660476",
+    "url": "/portfolio/static/css/main.6f7c6de9.chunk.css"
+  },
+  {
+    "revision": "64cde72354a8fc660476",
+    "url": "/portfolio/static/js/main.64cde723.chunk.js"
+  },
+  {
+    "revision": "bb2d15f8b100106d30b0",
+    "url": "/portfolio/static/js/1.bb2d15f8.chunk.js"
+  },
+  {
+    "revision": "7f12327ed5c20b198493",
+    "url": "/portfolio/static/js/2.7f12327e.chunk.js"
+  },
+  {
+    "revision": "f2236a703c23ce22eba2",
+    "url": "/portfolio/static/js/runtime~main.f2236a70.js"
+  },
+  {
+    "revision": "1ad59a28261ec68f4f8adfa043c57086",
+    "url": "/portfolio/index.html"
+  }
+];
